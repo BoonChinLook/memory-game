1a132d206cce17167a09999549e7925a05c4428f
BoonChinLook <lookbc30@gmail.com>
2026-01-04 22:03:41 +0000

    add new game button

--- app.js
+++ app.js
@@ -65,8 +65,7 @@ const cardArray = [
     }
 ]
 
-cardArray.sort(() => 0.5 - Math.random())
-
+cardArray.sort(() => 0.5 - Math.random());
 const gridDisplay = document.querySelector('#grid');
 let cardsChosen = [];
 let cardsChosenIds = [];
@@ -82,7 +81,18 @@ function createBoard() {
         gridDisplay.appendChild(card);
     }
 }
-createBoard();
+
+function newGame() {
+
+    gridDisplay.innerHTML = "";  
+    cardsChosen = [];
+    cardsChosenIds = [];
+    cardsWon.length = 0;
+    resultDisplay.textContent = "0";
+    cardArray.sort(() => 0.5 - Math.random()); 
+    createBoard();
+}
+
 
 function checkMatch() {
     const cards = document.querySelectorAll('#grid img');
@@ -120,6 +130,6 @@ function flipCard() {
     this.setAttribute('src', cardArray[cardId].img);
     
     if (cardsChosen.length === 2) {
-        setTimeout(checkMatch, 500);
+        setTimeout(checkMatch, 300);
     }
 }
